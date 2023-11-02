17b175c4e30769df5727a330e41554f2bcec7fa3
jeison sarria <jeison@goldenrace.com>
2023-11-01 22:19:22 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="turns-container"></div>
+      <div className="search-container"></div>
+      <div className="panel-container"></div>
+      <div className="statics-container"></div>
+    </div>
+  );
+}
+
+export default App;
